d4fd8a9efe654a57d8dab5a7b82e91aae4fd6f27
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2025-01-22 12:29:46 +0545

    adding view in auth modal

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,6 +1,32 @@
+import { AuthStrategy, ModalType } from '@/types/enums';
 import { View, Text } from 'react-native';
-import React from 'react';
-import { ModalType } from '@/types/enums';
+import googleImage from '@/assets/images/login/google.png';
+import microsoftImage from '@/assets/images/login/microsoft.png';
+import appleImage from '@/assets/images/login/apple.png';
+import slackImage from '@/assets/images/login/slack.png';
+
+const LOGIN_OPTIONS = [
+  {
+    text: 'Continue with Google',
+    icon: googleImage,
+    strategy: AuthStrategy.Google,
+  },
+  {
+    text: 'Continue with Microsoft',
+    icon: microsoftImage,
+    strategy: AuthStrategy.Microsoft,
+  },
+  {
+    text: 'Continue with Apple',
+    icon: appleImage,
+    strategy: AuthStrategy.Apple,
+  },
+  {
+    text: 'Continue with Slack',
+    icon: slackImage,
+    strategy: AuthStrategy.Slack,
+  },
+];
 
 interface AuthModalProps {
   authType: ModalType | null;
